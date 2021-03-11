36a3f76f5fb2aace165be7541f95a41be5600c39
melobern <melodie.bernard123@yahoo.fr>
2021-03-11 23:16:17 +0100

    Create secu.js

---
+++ secu.js
@@ -0,0 +1,29 @@
+//calcul secu : (97 - (2010199000111 % 97));
+
+const n = 999;
+var secuPremiers = 2010199000111; // Si doute sur les chiffres de la commune
+var secuDerniers = 2010199111000; // Si doute sur les chiffres du rang de naissance
+var cle = 01 // clé connue
+//var cle = (97 - (secu % 97)); => formule de calcul de la clé
+function bouclePre (secuPremiers) {
+    for (let i = 0; i < n; i++) {
+        secuPremiers += 1000;
+        cle = (97 - (secuPremiers % 97));
+      	if (cle ===39){
+        	console.log(secuPremiers + " " + cle);
+      		}
+      	else{};
+      }
+};
+function boucleDer (secuDerniers) {
+    for (let i = 0; i < n; i++) {
+        secuDerniers += 1;
+        cle = (97 - (secuDerniers % 97));
+      	if (cle ===39){
+        	console.log(secuDerniers + " " + cle);
+      		}
+      	else{};
+      }
+};
+bouclePre(secuPremiers); // Imprime tous les numéros de sécu où le doute était sur le département
+boucleDer(secuDerniers); // Imprime tous les numéros de sécu où le doute était sur le rang de naissance
