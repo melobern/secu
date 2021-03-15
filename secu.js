4d1167768c2818da4fd511e94bc8ddaf652f9204
melobern <46427692+melobern@users.noreply.github.com>
2021-03-15 21:45:23 +0100

    Update secu.js

--- secu.js
+++ secu.js
@@ -1,30 +1,35 @@
-//calcul secu : (97 - (2010199000111 % 97));
+var numSecu = 293116938704939;
+var numSansCle = numSecu.toString().slice(0,13);
+var laCle = (97 - (numSansCle % 97)); // Formule de calcul de la clé
+document.write(numSansCle.toString() + laCle.toString() + "<BR>");
+document.write("La clé de " + numSansCle + "  est  " + laCle +  "<BR><BR>");
 
+var ntS = numSecu.toString();
 const n = 999;
-var secuPremiers = 2010199000111; // Si doute sur les chiffres de la commune
-var secuDerniers = 2010199111000; // Si doute sur les chiffres du rang de naissance
-var cle = 01 // clé connue
-//var cle = (97 - (secu % 97)); => formule de calcul de la clé
-function bouclePre (secuPremiers) {
-    for (let i = 0; i < n; i++) {
-        secuPremiers += 1000;
-        cle = (97 - (secuPremiers % 97));
-      	if (cle ===39){
-        	document.write(secuPremiers + " " + cle +  "<BR>");
-      		}
-      	else{};
-      }
-};
-function boucleDer (secuDerniers) {
-    for (let i = 0; i < n; i++) {
-        secuDerniers += 1;
-        cle = (97 - (secuDerniers % 97));
-      	if (cle ===39){
-        	document.write(secuDerniers + " " + cle +  "<BR>");
-      		}
-      	else{};
-      }
-};
-bouclePre(secuPremiers); // Imprime tous les numéros de sécu où le doute était sur le département
-boucleDer(secuDerniers); // Imprime tous les numéros de sécu où le doute était sur le rang de naissance
+var chifDep = parseInt(ntS.slice(0,7)+ "000" + ntS.slice(10,13));
+var rangNe =  parseInt(ntS.slice(0,10)+ "000");
+var cleConnue = parseInt(ntS.slice(13,15));
 
+function bouclePre (x) {
+    for (let i = 0; i < n; i++) {
+        x += 1000;
+        cle2 = (97 - (x % 97));
+      	if (cle2 === cleConnue){
+        	document.write(x + " " + cleConnue +  "<BR>");
+      		}
+      	else{};
+      }
+};
+function boucleDer (x) {
+    for (let i = 0; i < n; i++) {
+        x += 1;
+        cle2 = (97 - (x % 97));
+      	if (cle2  === cleConnue){
+        	document.write(x + " " + cleConnue +  "<BR>");
+      		}
+      	else{};
+      }
+};
+bouclePre(chifDep); // Imprime numéros de sécu où le doute est sur le département
+document.write("<BR>");
+boucleDer(rangNe); // Imprime numéros de sécu où le doute est sur le rang de naissance
